0f9a670d41fde94bb98db61cb0d6f7bcd0ba3b6c
shdxiang <shdxiang@qq.com>
2016-01-22 05:12:36 +0000

    Add gpio control

--- power-plug.py
+++ power-plug.py
@@ -4,15 +4,20 @@ import time
 import sys
 import json
 import logging
+import RPi.GPIO as GPIO
 from messenger import Messenger
 
 ALIAS = 'power_plug'
+GPIO_NUM = 4
 
 class PowerPlug(Messenger):
 
-    def __init__(self, alias):
+    def __init__(self, alias, gpio_num):
         logging.debug('init')
         Messenger.__init__(self, alias)
+
+        self.gpio_num = gpio_num
+        GPIO.setup(self.gpio_num, GPIO.OUT)
 
     def __del__(self):
         logging.debug('del')
@@ -27,15 +32,21 @@ class PowerPlug(Messenger):
 
         if msg['m'] == 'on':
             logging.debug('on')
+            GPIO.output(self.gpio_num, GPIO.HIGH)
         elif msg['m'] == 'off':
             logging.debug('off')
+            GPIO.output(self.gpio_num, GPIO.LOW)
 
 if __name__ == '__main__':
 
     #logging.getLogger('requests').setLevel(logging.WARNING)
     logging.basicConfig(level=logging.DEBUG)
 
-    plug = PowerPlug(ALIAS)
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_NUM, GPIO.OUT)
+
+    plug = PowerPlug(ALIAS, GPIO_NUM)
 
     while True:
         plug.loop()
