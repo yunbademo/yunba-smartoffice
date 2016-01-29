249e37a54762cb528607e7b6f1e7289fda75937f
shdxiang <shdxiang@qq.com>
2016-01-29 15:05:08 +0800

    Add test files for arduino plug

---
+++ test/arduino_power_plug_on.sh
@@ -0,0 +1,11 @@
+if [ $# != 1 ] ; then
+  echo usage: `basename ${0}` '<ALIAS>'
+  exit 1
+fi
+
+alias="${1}"
+msg="{\\\"cmd\\\": \\\"plug_set\\\", \\\"devid\\\": \\\"${alias}\\\", status\\\": 1}"
+
+#echo "${msg}"
+./publish.sh "${alias}" "${msg}"
+
