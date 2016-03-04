2c2eec6b143dfb1840fe533b73c2b3b9543b2658
shdxiang <shdxiang@qq.com>
2016-03-04 13:13:42 +0800

    Add sketch_switch.ino

---
+++ arduino/sketch_switch/sketch_switch.ino
@@ -0,0 +1,98 @@
+/* serial data format:
+ *  header:
+ *    flag: 1 byte, 0xaa, filter out debug message
+ *    type: 1 bytes, 1: upstream, 2: downstream
+ *    body length: 2 bytes, network byte order
+ *  body:
+ *    json data from yunba service
+ */ 
+
+#define HEADER_LEN 4
+#define BUF_LEN 256
+#define FLAG_CHAR 0xaa
+
+const char *g_appkey = "5697113d4407a3cd028abead";
+const char *g_topic = "smart_office";
+const char *g_devid = "switch_0";
+
+char g_header[HEADER_LEN];
+char g_buf[BUF_LEN];
+int g_step = 1; // 1: recv header, 2 recv body
+uint16_t g_body_len = 0;
+uint16_t g_recv_len = 0;
+
+void recv_header() {
+  while (Serial.available() >= HEADER_LEN) {
+    Serial.readBytes(g_header, 1);
+    if (g_header[0] == FLAG_CHAR) {
+      break;
+    }
+  }
+
+  if (g_header[0] != FLAG_CHAR) { // no enough data
+    return;
+  }
+
+  Serial.readBytes(g_header + 1, HEADER_LEN - 1);
+
+  if (g_header[1] != 2) { // not a downstream message
+    return;
+  }
+
+  /* le */
+  ((uint8_t *)&g_body_len)[0] = g_header[4];
+  ((uint8_t *)&g_body_len)[1] = g_header[3];
+
+  if (g_body_len > (BUF_LEN - HEADER_LEN - 1)) {
+    Serial.println("too long msg");
+    return;
+  }
+
+  g_recv_len = 0;
+  g_step = 2;
+}
+
+void recv_body() {
+  uint16_t len = 0;
+
+  len = Serial.readBytes(g_buf + g_recv_len, g_body_len - g_recv_len);
+  g_recv_len += len;
+
+  if (g_recv_len != g_body_len) {
+    return;
+  }
+
+  // now got a completed msg
+  g_buf[g_body_len] = 0;
+  Serial.println("got a msg:");
+  Serial.println(g_buf);
+
+  handle_msg();
+
+  g_step = 1;
+}
+
+void handle_msg() {
+  
+}
+
+void handle_input() {
+  if (g_step == 1) {
+    recv_header();
+  } else if (g_step == 2) {
+    recv_body();
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  //Serial.setTimeout(100);
+  Serial.println("st.."); // setup
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  handle_input();
+  delay(10);
+}
