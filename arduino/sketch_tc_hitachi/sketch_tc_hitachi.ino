613dff5452ee42f1d6a990d667f5085d1b83f81b
shdxiang <shdxiang@qq.com>
2016-04-13 12:57:35 +0800

    Fix read LCD info

--- arduino/sketch_tc_hitachi/sketch_tc_hitachi.ino
+++ arduino/sketch_tc_hitachi/sketch_tc_hitachi.ino
@@ -22,11 +22,11 @@
 #define BUF_LEN 256
 #define FLAG_CHAR 0xaa
 
-#define COM_NUM 3
-#define SEG_NUM 5
+#define COM_NUM 4
+#define SEG_NUM 4
 
 #define COM_PIN A0
-#define SEG_PIN A3
+#define SEG_PIN A4
 
 #define PIN_ON_OFF 9
 #define PIN_MODE 10
@@ -68,6 +68,9 @@ int8_t g_room_temp;
 
 uint8_t g_stable_cnt = 0; /* 稳定次数 */
 uint8_t g_stable_cnt_slave = 0;
+
+uint8_t g_last_unit = 5; /* 上一个个位数，默认 25 度 */
+uint8_t g_last_ten = 2; /* 上一个十位数，默认 25 度 */
 
 void recv_header() {
   while (Serial.available() >= HEADER_LEN) {
@@ -217,71 +220,57 @@ int make_number(int seg_index) {
   uint8_t j = 0;
   uint8_t k = 0;
 
-  /* 十位: BCD */
-  i = g_data[0][seg_index + 2];
-  i += g_data[1][seg_index + 2] * 2;
-  i += g_data[2][seg_index + 2] * 4;
-
-  switch (i) {
-    case 0x03:
-      j = 1;
-      break;
-    case 0x05:
-      j = 2;
-      break;
-    case 0x07:
-      j = 3;
-      break;
-    default:
-      //j = 0;
-      //break;
-      return -1;
-  }
-
-  /* 个位: BCDFGE */
+  /* 个位: BCDAFGE */
   i = g_data[0][seg_index];
-  i += g_data[1][seg_index] * 2;
-  i += g_data[2][seg_index] * 4;
-  i += g_data[0][seg_index + 1] * 8;
-  i += g_data[1][seg_index + 1] * 16;
-  i += g_data[2][seg_index + 1] * 32;
+  i += g_data[1][seg_index] << 1;
+  i += g_data[2][seg_index] << 2;
+  i += g_data[3][seg_index] << 3;
+  i += g_data[0][seg_index + 1] << 4;
+  i += g_data[1][seg_index + 1] << 5;
+  i += g_data[2][seg_index + 1] << 6;
 
   switch (i) {
-    case 0x2f:
+    case 0x5f:
       k = 0;
       break;
     case 0x03:
       k = 1;
       break;
-    case 0x35:
+    case 0x6d:
       k = 2;
       break;
-    case 0x17:
+    case 0x2f:
       k = 3;
       break;
-    case 0x1b:
+    case 0x33:
       k = 4;
       break;
-    case 0x1e:
+    case 0x3e:
       k = 5;
       break;
-    case 0x3e:
+    case 0x7e:
       k = 6;
       break;
-    case 0x0b:
+    case 0x1b:
       k = 7;
       break;
-    case 0x3f:
+    case 0x7f:
       k = 8;
       break;
-    case 0x1f:
+    case 0x3f:
       k = 9;
       break;
     default:
       return -1;
   }
+  if (k == 0 && g_last_unit == 9) {
+    g_last_ten++;
+  } else if (k == 9 && g_last_unit == 0){
+    g_last_ten--;
+  }
+  g_last_unit = k; /* 记下上一个个位数 */
 
-  return j * 10 + k;
+  return g_last_ten * 10 + k;
 }
 
 void handle_status() {
@@ -351,7 +340,7 @@ void handle_status() {
     return;
   }
 
-#if 1
+#if 0
   for (i = 0; i < COM_NUM; i++) {
     for (j = 0; j < SEG_NUM; j++) {
       Serial.print(g_data[i][j], DEC);
@@ -361,27 +350,32 @@ void handle_status() {
 #endif
 
   /* g_mode */
-  if (g_data[0][4]) {
+  if (g_data[0][3]) {
     g_mode = 1;
-  } else if (g_data[1][4]) {
+  } else if (g_data[1][3]) {
     g_mode = 2;
-  } else if (g_data[2][4]) {
-    g_mode = 3;
-  } else {
+  } else if (g_data[3][3]) {
     g_mode = 4;
+  } else {
+    g_mode = 3;
   }
 
   /* g_fan */
-  if (g_data[0][3]) {
+  if (g_data[0][2]) {
     g_fan = 2;
-  } else if (g_data[1][3]) {
+  } else if (g_data[1][2]) {
     g_fan = 3;
-  } else {
+  } else if (g_data[3][2]) {
     g_fan = 1;
+  } else {
+    g_fan = 0;
   }
 
   /* g_set_temp */
   g_set_temp = make_number(0);
+  if (g_set_temp < 0) {
+    g_set_temp = 0;
+  }
   g_need_report = 1;
 }
 
